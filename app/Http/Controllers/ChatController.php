51b6076e91bc3a1dde390071a3a78f2a9d4f7c3b
Ashish Ginotra <ashish@headerlabs.com>
2015-09-16 17:24:23 +0530

    retchet implement

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -1,89 +1,45 @@
 <?php
-
 namespace App\Http\Controllers;
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
 
-use Illuminate\Http\Request;
-use Input;
-use Pusher;
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
+class ChatController extends Controller implements MessageComponentInterface {
+    protected $clients;
 
-class ChatController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return Response
-     */
-    public function index()
-    {
-        //
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function create()
-    {
+    public function onOpen(ConnectionInterface $conn) {
+        // Store the new connection to send messages to later
+        $this->clients->attach($conn);
 
+        echo "New connection! ({$conn->resourceId})\n";
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  Request  $request
-     * @return Response
-     */
-    public function store()
-    {
-        $pusher = new Pusher( 'f52f0d0ddf0322ff7661', 'b8bf77e415c0c57e1921', '140367' );
-        $pusher->trigger( Input::get('channel'), Input::get('event'), Input::get('message') );
+    public function onMessage(ConnectionInterface $from, $msg) {
+        $numRecv = count($this->clients) - 1;
+        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
+            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
+
+        foreach ($this->clients as $client) {
+            if ($from !== $client) {
+                // The sender is not the receiver, send to each client connected
+                $client->send($msg);
+            }
+        }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function show($id)
-    {
-        //
+    public function onClose(ConnectionInterface $conn) {
+        // The connection is closed, remove it, as we can no longer send it messages
+        $this->clients->detach($conn);
+
+        echo "Connection {$conn->resourceId} has disconnected\n";
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        echo "An error has occurred: {$e->getMessage()}\n";
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  Request  $request
-     * @param  int  $id
-     * @return Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        //
+        $conn->close();
     }
 }
